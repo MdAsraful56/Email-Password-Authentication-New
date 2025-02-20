f2829c8d4a78dec61737178652de56c919a649de
MdAsraful56 <work.mdasraful56@gmail.com>
2025-02-20 23:12:15 +0600

    navbar completed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,7 @@
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <h1 className="text-center text-4xl justify-center items-center">Home</h1>
         </div>
     );
 };
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router";
+import Navber from "../Navber/Navber";
 
 
 const Root = () => {
     return (
         <div>
+            <Navber />
             <Outlet />
         </div>
     );
